ed7dab86c45716aaa4208e3831b94c5581202503
KeithDylGH <KeithDylTM@gmail.com>
2025-12-19 21:42:42 -0400

    gardenV2 2.0.3

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -21,7 +21,7 @@ const config: CapacitorConfig = {
     },
     LiveUpdates: {
       appId: 'b8a00d75', 
-      channel: 'Production',
+      channel: 'production',
       autoUpdateMethod: 'background',
       maxVersions: 2 // Recomendado para no llenar el almacenamiento del usuario
     }
